bebb7d078fb9bbd4fe14aa799012a11828bfd64a
Jeremy Somsouk <jeremy@somsouk.fr>
2025-11-01 21:35:59 +0100

    update testcontainers ports

--- tests/integration/infrastructure/adapter/outbound/email/test_mailhog_email_sender_integration.py
+++ tests/integration/infrastructure/adapter/outbound/email/test_mailhog_email_sender_integration.py
@@ -11,15 +11,15 @@ class TestMailhogIntegration:
     def test_send_activation_email(self, email_sender, mailhog_container):
         # Given
         test_email = Email("user@spookymotion.com")
-        activation_code = "123456"
-        requests.delete("http://localhost:8025/api/v1/messages")
+        activation_code = "1234"
+        requests.delete("http://localhost:8024/api/v1/messages")
 
         # When
         email_sender.send_activation_email(test_email, activation_code)
 
         # Then
         time.sleep(1)
-        response = requests.get("http://localhost:8025/api/v2/messages")
+        response = requests.get("http://localhost:8024/api/v2/messages")
         assert response.status_code == 200
 
         emails = response.json()
@@ -31,4 +31,4 @@ class TestMailhogIntegration:
         assert email_data["From"]["Mailbox"] == "noreply"
         assert email_data["From"]["Domain"] == "spookymotion.com"
         assert email_data["Content"]["Headers"]["Subject"][0] == "Activate Your Account"
-        assert "Your activation code is: 123456" in email_data["Content"]["Body"]
+        assert "Your activation code is: 1234" in email_data["Content"]["Body"]
